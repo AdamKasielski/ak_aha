1bfc65115fa6b6381e4a31a717776e6b0ba0178f
AdamKasielski <159795461+AdamKasielski@users.noreply.github.com>
2025-03-20 12:27:51 +0100

    Update default.sh

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec python3 -m "my_package.my_script_test"
+dt-exec python3 -m "packages.my_package.my_script_test"
 
 
 # ----------------------------------------------------------------------------
